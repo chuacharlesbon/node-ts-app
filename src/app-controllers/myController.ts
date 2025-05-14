ce743bbf28871a491f76c933b827132898cfc021
Charles Bon Chua <chuacharlesbon@gmail.com>
2025-05-14 10:35:54 +0800

    Added: MySQL

--- src/app-controllers/myController.ts
+++ src/app-controllers/myController.ts
@@ -1,12 +1,24 @@
 import { Request, Response } from 'express';
+import pool from '../config/db';
 
 const myController1 = (req: Request, res: Response): void => {
     console.log("My controller req.method", req.method);
     res.status(200).json({ message: "Connected to my controller 1" });
 };
 
-const myController2 = (req: Request, res: Response): void => {
+const myController2 = async (req: Request, res: Response) => {
     console.log("My controller req.method", req.method);
+
+    try {
+        const [rows] = await pool.query('SELECT * FROM users');
+        res.json(rows);
+    } catch (err) {
+        console.error('DB error:', err);
+        res.status(500).json({
+            error: 'Server error',
+            errorData: err
+        });
+    }
     res.status(200).json({ message: "Connected to my controller 1" });
 };
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import myRouter from './app-routes/myRoute';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 
 const port = process.env.PORT ?? 4000;
